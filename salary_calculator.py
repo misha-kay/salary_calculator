0dc95d1c28fb30ae2e29ea7d9bc9fff51f355c37
misha-kay <misha.kay92@gmail.com>
2023-11-22 10:14:50 +0000

    Bug fixes and adjustments to functions

--- salary_calculator.py
+++ salary_calculator.py
@@ -1,138 +1,268 @@
 import re
 
-# -------------- USER INPUT CONTROL -------------- #
-
-annualSalary = float(input("What is your gross annual salary? £"))
-taxCode = input("What is your tax code? ")
-slPlan = int(
-    input(
-        "Please enter your student loan:\n'1' for Plan 1\n'2' for Plan 2\n'4' for Plan 4\n'5' for Plan 5\n'6' for Postgraduate Loan\n'0' if you don't have one\n"
-    )
-)
-pensionType = input(
-    "Please enter your pension type:\n'auto' for auto-enrolment\n'sac' for salary sacrifice\n'none' if you don't have one\n"
-).lower()
-pensionPercentage = int(input("What is your pension payment percentage? "))
-
-# -------------- GLOBAL CONSTS & VARIABLES -------------- #
-
-grossPay = round(annualSalary / 12, 2)
-taxRate = 0
-slSalary = 0
-pensionPercentageConverted = float(pensionPercentage / 100)
 PT = 12576
 LEL = 6240
 UEL = 50270
 NICUEL = 50268
 
-# -------------- WORK OUT TAX-FREE ALLOWANCE -------------- #
 
-if taxCode == "D0":
-    taxRate = 0.40
-elif taxCode == "D1":
-    taxRate = 0.45
-else:
-    taxRate = 0.20
+def get_annual_salary():
+    while True:
+        annual_salary = input("What is your gross annual salary? £")
+        try:
+            if not annual_salary.isnumeric():
+                raise ValueError(
+                    "Please enter a valid number. Symbols and letters are not accepted."
+                )
+            return float(annual_salary)
+        except ValueError as e:
+            print(e)
 
-# User not entitled to tax-free allowance - Taxed on full gross pay
-# Make tax code blank
-(taxCodeChars, count) = re.subn("BR|0T|D0|D1", "", taxCode, flags=re.IGNORECASE)
-# Set tax-free amount to zero
-taxFree = taxCodeChars + "0" * count
 
-# If user does not have one of the above taxcodes, user is entitled to tax-free allowance, so run the following:
-if count == 0:
-    # Remove the letters below from the taxcode and replace with an empty character. Count how many times this was actioned
-    (taxCodeDigits, count) = re.subn("K|L|M|N|T", "", taxCode, flags=re.IGNORECASE)
-    # Add a zero to the end of the number to get tax-free amount
-    taxFree = taxCodeDigits + "0" * count
+def get_tax_code():
+    while True:
+        tax_code = input("What is your tax code? ").upper()
+        valid_letters = ["K", "L", "M", "N", "T", "BR", "0T", "D0", "D1"]
 
-# Check for invalid taxcodes
-if taxCode == taxFree:
-    print("Invalid taxcode")
+        try:
+            if not any(letter in tax_code for letter in valid_letters):
+                raise ValueError("Please provide a valid tax code.")
+            return tax_code
+        except ValueError as e:
+            print(e)
 
-taxablePay = annualSalary - int(taxFree)
 
-# -------------- WORK OUT PENSION DUE -------------- #
+def get_student_loan_plan():
+    while True:
+        student_loan_plan = input(
+            """
+Please enter your student loan:\n
+'1' for Plan 1
+'2' for Plan 2
+'4' for Plan 4
+'5' for Plan 5
+'6' for Postgraduate Loan
+'0' if you don't have one
+"""
+        )
+        valid_numbers = ["0", "1", "2", "3", "4", "5", "6"]
+        try:
+            if (
+                student_loan_plan not in valid_numbers
+                or not student_loan_plan.isdigit()
+                or int(student_loan_plan) > 6
+            ):
+                raise ValueError(
+                    "Please enter a valid single-digit number. Symbols and letters are not accepted."
+                )
+            return int(student_loan_plan)
+        except ValueError as e:
+            print(e)
 
-if pensionType == "auto" and annualSalary >= LEL and annualSalary < UEL:
-    annualPensionable = annualSalary - LEL
-    annualPension = annualPensionable * pensionPercentageConverted
-    pension = annualPension / 12
-    # Adjustment for taxable pay and student loan
-    taxablePay -= annualPension
-    slSalary = annualSalary
-    print(taxablePay)
 
-elif pensionType == "auto" and annualSalary >= UEL:
-    annualPensionable = UEL - LEL
-    annualPension = annualPensionable * pensionPercentageConverted
-    pension = annualPension / 12
-    # Adjustment for taxable pay and student loan
-    taxablePay -= annualPension
-    slSalary = annualSalary
-else:
-    pension = 0
-if pensionType == "sac":
-    annualPension = annualSalary * pensionPercentageConverted
-    # Adjustment for taxable pay and student loan
-    taxablePay -= annualPension
-    pension = annualPension / 12
-    slSalary = annualSalary - annualPension
+def get_pension_type():
+    while True:
+        pension_type = input(
+            """
+Please enter your pension type:
+'auto' for auto-enrolment
+'sac' for salary sacrifice
+'none' if you don't have one
+"""
+        ).lower()
+        valid_answers = ["auto", "sac", "none"]
+        try:
+            if pension_type not in valid_answers:
+                raise ValueError("Please enter a valid option from the list.")
+            return pension_type
+        except ValueError as e:
+            print(e)
 
-pension = round(pension, 2)
 
-# -------------- WORK OUT NICs DUE -------------- #
+def get_pension_percentage():
+    while True:
+        pension_percentage = input("What is your pension percentage? ")
+        try:
+            if not pension_percentage.isnumeric():
+                raise ValueError("Please enter a number (eg: 3% = '3')")
+            return float(pension_percentage) / 100
+        except ValueError as e:
+            print(e)
 
-print(annualSalary, pensionType, taxablePay)
 
-if annualSalary >= PT and annualSalary < NICUEL and pensionType == "auto":
-    nationalInsurance = (taxablePay + annualPension) * 0.12 / 12
-elif annualSalary >= PT and annualSalary < NICUEL and pensionType == "sac":
-    nationalInsurance = (taxablePay) * 0.12 / 12
-elif annualSalary >= NICUEL:
-    perc12 = (NICUEL - PT) * 0.12
-    perc2 = (annualSalary - NICUEL) * 0.02
-    nationalInsurance = (perc12 + perc2) / 12
+def calculate_taxable_pay(tax_code, annual_salary):
+    if tax_code == "D0":
+        tax_rate = 0.40
+    elif tax_code == "D1":
+        tax_rate = 0.45
+    else:
+        tax_rate = 0.20
 
-print(taxablePay)
-nationalInsurance = round(nationalInsurance, 2)
+    # User not entitled to tax-free allowance - Taxed on full gross pay
+    # REGX - Make tax code blank
+    (tax_code_chars, count) = re.subn("BR|0T|D0|D1", "", tax_code, flags=re.IGNORECASE)
+    # Set tax-free amount to zero
+    tax_free = tax_code_chars + "0" * count
 
-# -------------- WORK OUT TAX DUE -------------- #
+    # REGX - If user does not have one of the above taxcodes, user is entitled to tax-free allowance, so run the following:
+    if count == 0:
+        # Remove the letters below from the taxcode and replace with an empty character. Count how many times this was actioned
+        (tax_code_digits, count) = re.subn(
+            "K|L|M|N|T", "", tax_code, flags=re.IGNORECASE
+        )
+        # Add a zero to the end of the number to get tax-free amount
+        tax_free = tax_code_digits + "0" * count
+        tax_free = int(tax_free)
+    if annual_salary < tax_free:
+        return (0, 0)
 
-tax = round((taxablePay * taxRate) / 12, 2)
+    return (round(annual_salary - tax_free, 2), tax_rate)
 
-# -------------- WORK OUT STUDENT LOAN DUE -------------- #
 
-if slPlan == 0:
-    studentLoan = 0
-elif slPlan == 1 and slSalary < 22015:
-    studentLoan = 0
-elif slPlan == 1 and slSalary >= 22015:
-    studentLoan = (slSalary - 22015) * 0.09 / 12
-elif slPlan == 2 and slSalary < 27295:
-    studentLoan = 0
-elif slPlan == 2 and slSalary >= 27295:
-    studentLoan = (slSalary - 27295) * 0.09 / 12
-elif slPlan == 4 and slSalary < 27660:
-    studentLoan = 0
-elif slPlan == 4 and slSalary >= 27660:
-    studentLoan = (slSalary - 27660) * 0.09 / 12
-elif slPlan == 5 and slSalary < 25000:
-    studentLoan = 0
-elif slPlan == 5 and slSalary >= 25000:
-    studentLoan = (slSalary - 25000) * 0.09 / 12
-elif slPlan == 6 and slSalary < 21000:
-    studentLoan = 0
-elif slPlan == 6 and slSalary >= 21000:
-    studentLoan = (slSalary - 21000) * 0.06 / 12
+def calculate_pension(
+    pension_type, annual_gross_salary, pension_percentage, taxable_pay
+):
+    annual_pension, student_loan_salary = 0, 0
+    if (
+        pension_type == "auto"
+        and annual_gross_salary >= LEL
+        and annual_gross_salary < UEL
+    ):
+        annual_pensionable = annual_gross_salary - LEL
+        annual_pension = annual_pensionable * pension_percentage
+        # Adjustment for taxable pay and student loan
+        taxable_pay -= annual_pension
+        student_loan_salary = annual_gross_salary
+    elif pension_type == "auto" and annual_gross_salary >= UEL:
+        annual_pensionable = UEL - LEL
+        annual_pension = annual_pensionable * pension_percentage
+        # Adjustment for taxable pay and student loan
+        taxable_pay -= annual_pension
+        student_loan_salary = annual_gross_salary
+    elif pension_type == "sac":
+        annual_pension = annual_gross_salary * pension_percentage
+        # Adjustment for taxable pay and student loan
+        taxable_pay -= annual_pension
+        student_loan_salary = annual_gross_salary - round(annual_pension, 2)
+    else:
+        print("Error: Could not calculate pension")
 
-studentLoan = int(studentLoan)
+    return (round(annual_pension, 2), round(student_loan_salary, 2))
 
-# -------------- WORK OUT NET PAY -------------- #
 
-netPay = round(grossPay - tax - nationalInsurance - pension - studentLoan, 2)
+def calculate_NIC(gross_annual_salary, pension_type, taxable_pay, annual_pension):
+    national_insurance = 0
+    print("Initial NI: ", national_insurance)
+    if (
+        gross_annual_salary >= PT
+        and gross_annual_salary < NICUEL
+        and pension_type == "auto"
+    ):
+        national_insurance = ((gross_annual_salary - PT) * 0.12) / 12
+        print("Final NI: ", national_insurance)
+    elif (
+        gross_annual_salary >= PT
+        and gross_annual_salary < NICUEL
+        and pension_type == "sac"
+    ):
+        national_insurance = (taxable_pay * 0.12) / 12
+    elif gross_annual_salary >= NICUEL:
+        percent12 = (NICUEL - PT) * 0.12
+        percent2 = gross_annual_salary - NICUEL * 0.02
+        national_insurance = (percent12 + percent2) / 12
+    else:
+        print("Error: Could not calculate NICs")
 
-print(
-    f"Your payslip this month should be as follows:\nGross Pay:  £{grossPay}\nTax: £{tax}\nNational Insurance: £{nationalInsurance}\nPension: £{pension}\nStudent Loan: £{studentLoan}\nNet Pay: £{netPay}\n\nTherefore, your take home pay is £{netPay}\n"
-)
+    return round(national_insurance, 2)
+
+
+def calculate_tax(taxable_pay, tax_rate, annual_pension):
+    return round(((taxable_pay - annual_pension) * tax_rate) / 12, 2)
+
+
+def calculate_student_loan(student_loan_plan, student_loan_salary):
+    plan1_threshold = 22015
+    plan2_threshold = 27295
+    plan4_threshold = 27660
+    plan5_threshold = 25000
+    postgrad_threshold = 21000
+    standard_rate = 0.09
+    postgrad_rate = 0.06
+
+    if student_loan_plan == 0:
+        return 0
+    elif student_loan_plan == 1 and student_loan_salary < plan1_threshold:
+        return 0
+    elif student_loan_plan == 1 and student_loan_salary >= plan1_threshold:
+        return int((student_loan_salary - plan1_threshold) * standard_rate / 12)
+    elif student_loan_plan == 2 and student_loan_salary < plan2_threshold:
+        return 0
+    elif student_loan_plan == 2 and student_loan_salary >= plan2_threshold:
+        return int((student_loan_salary - plan2_threshold) * standard_rate / 12)
+    elif student_loan_plan == 4 and student_loan_salary < plan4_threshold:
+        return 0
+    elif student_loan_plan == 4 and student_loan_salary >= plan4_threshold:
+        return int((student_loan_salary - plan4_threshold) * standard_rate / 12)
+    elif student_loan_plan == 5 and student_loan_salary < plan5_threshold:
+        return 0
+    elif student_loan_plan == 5 and student_loan_salary >= plan5_threshold:
+        return int((student_loan_salary - plan5_threshold) * standard_rate / 12)
+    elif student_loan_plan == 6 and student_loan_salary < postgrad_threshold:
+        return 0
+    elif student_loan_plan == 6 and student_loan_salary >= postgrad_threshold:
+        return int((student_loan_salary - postgrad_threshold) * postgrad_rate / 12)
+    else:
+        print("Error: Could not calculate student loan repayment")
+        return 0
+
+
+def main():
+    annual_gross = 0
+    monthly_gross = 0
+    tax_code = 0
+    student_loan_plan = 0
+    pension_type = 0
+    pension_percentage = 0
+
+    annual_gross = get_annual_salary()
+    monthly_gross = annual_gross / 12
+    tax_code = get_tax_code()
+    student_loan_plan = get_student_loan_plan()
+    pension_type = get_pension_type()
+    if pension_type != "none":
+        pension_percentage = get_pension_percentage()
+
+    (taxable_pay, tax_rate) = calculate_taxable_pay(tax_code, annual_gross)
+
+    (annual_pension, student_loan_salary) = calculate_pension(
+        pension_type, annual_gross, pension_percentage, taxable_pay
+    )
+
+    national_insurance = calculate_NIC(
+        annual_gross, pension_type, taxable_pay - annual_pension, annual_pension
+    )
+
+    tax = calculate_tax(taxable_pay, tax_rate, annual_pension)
+    student_loan = calculate_student_loan(student_loan_plan, student_loan_salary)
+
+    monthly_pension = annual_pension / 12
+
+    net_pay = round(
+        monthly_gross - tax - national_insurance - monthly_pension - student_loan, 2
+    )
+
+    print(
+        f"""
+    Your payslip this month should be as follows:\n
+    Gross Pay:  £{round(monthly_gross,2)}\n
+    Tax: £{abs(tax)}\n
+    National Insurance: £{national_insurance}\n
+    Pension: £{round(monthly_pension,2)}\n
+    Student Loan: £{student_loan}\n
+    Net Pay: £{net_pay}\n\n
+    Therefore, your take home pay is £{net_pay}\n
+    """
+    )
+
+
+main()
